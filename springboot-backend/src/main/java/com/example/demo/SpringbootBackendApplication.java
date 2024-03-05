0403190806c44e4c9aa09a6cad9dfae197600ba6
Deepika <deepikab@cybage.com>
2024-03-05 16:09:17 +0530

    initial state

---
+++ springboot-backend/src/main/java/com/example/demo/SpringbootBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	     // String port=System.getenv("PORT");
+	     // String host=System.getenv("HOST");
+	     // String mysql_user=System.getenv("MYSQL_USER");
+	     // String mysql_password=System.getenv("MYSQL_PASSWORD");
+	}
+
+}
